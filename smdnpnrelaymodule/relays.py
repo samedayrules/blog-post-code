5f87267bef5c5b574773a82245e32fedc6a54547
Customer Service <104595407+samedayrules@users.noreply.github.com>
2022-04-29 11:21:21 -0700

    Create relays.py
    Initial commit

---
+++ smdnpnrelaymodule/relays.py
@@ -0,0 +1,24 @@
+# Originally posted:
+# https://samedayrules.com/using-smd-npn-relay-board
+
+import RPi.GPIO as GPIO # access to GPIO pins
+import time # use this to pause execution below
+
+pins = [2, 3, 4, 17, 27, 22, 10, 9] # list of pins we'll be using
+GPIO.setmode(GPIO.BCM) # reference pins using logical pin numbers
+GPIO.setup(pins, GPIO.OUT) # set all pins to output mode
+GPIO.output(pins, GPIO.LOW) # set all pins low (de-activates relays)
+
+# Loop through pins, turning them ON, pausing, then OFF
+for p in pins:
+  GPIO.output(p, GPIO.HIGH) # relay associated with this pin should now be ON
+  time.sleep(3) # pause for 3 seconds
+  GPIO.output(p, GPIO.LOW) # relay should now be OFF
+
+# Turn all the relays ON and then OFF
+GPIO.output(pins, GPIO.HIGH)
+time.sleep(3)
+GPIO.output(pins, GPIO.LOW)
+
+#Cleanup
+GPIO.cleanup()
